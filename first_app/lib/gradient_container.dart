6fa427a464f673359d63c06435139dadbc7a2ada
electronicmaxwell <max.102301@gmail.com>
2024-09-18 14:52:54 +0200

    Complete first app

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/dice_roller.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: colors,
+        ),
+      ),
+      child: const Center(
+          child: DiceRoller()),
+    );
+  }
+}
